658529eb54c3197eabe70fc190849cbf4f3ed671
linhsol26 <nhglinh.sol@gmail.com>
2021-04-11 12:23:53 +0700

    camera follow char

---
+++ Assets/Scripts/Camera Scripts/CameraFollow.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    private Transform player;
+
+    public float minX, maxX;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player)
+        {
+            Vector3 temp = transform.position;
+            temp.x = player.position.x;
+
+            if (temp.x < minX)
+            {
+                temp.x = minX;
+            }
+
+            if (temp.x > maxX)
+            {
+                temp.x = maxX;
+            }
+
+            transform.position = temp;
+        }
+    }
+}
